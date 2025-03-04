ff6266d89b0b2b5baf04c7f6b3648bbbb820b990
bbarrington0099 <bbarrington0099@gmail.com>
2025-03-03 21:18:51 -0600

    main takes arg in cli

--- main.py
+++ main.py
@@ -1,59 +1,41 @@
-def get_book_content(book_path) :
-    with open(book_path) as book:
-        book_content = book.read()
-        return book_content
+import sys
+from stats import (
+    get_num_words,
+    chars_dict_to_sorted_list,
+    get_chars_dict,
+)
 
-def count_book_words(book_content) :
-    words = book_content.split()
-    return len(words)
-    
-def count_book_character_occurence(book_content) :
-    character_occurences = {}
-    book_content_santized = book_content.lower()
-    for character in book_content_santized :
-        if character in character_occurences :
-            character_occurences[character] += 1
-        else :
-            character_occurences[character] = 1
-    return character_occurences
 
-def sort_filter_character_occurence(character_occurences) :
-    character_dictionaries = []
-    for character_occurence in character_occurences :
-        character_dictionaries.append({"character" : character_occurence, "count" : character_occurences[character_occurence]})
-    filtered_dictionaries = filter_alphabet(character_dictionaries)
-    filtered_dictionaries.sort(reverse=True, key=sort_count)
-    return filtered_dictionaries
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
 
-def generate_book_report(book_path) :
-    book_content = get_book_content(book_path)
-    word_count = count_book_words(book_content)
-    character_occurences = count_book_character_occurence(book_content)
-    character_dictionaries = sort_filter_character_occurence(character_occurences)
-    book_report = ""
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
 
-    book_report += f"--- Begin report of {book_path} ---\n"
-    book_report += f"{word_count} words found in the document\n\n"
-    
-    for character_dictionary in character_dictionaries :
-        character = character_dictionary["character"]
-        count = character_dictionary["count"]
-        book_report += f"The '{character}' character was found {count} times\n"
-    book_report += "--- End report ---"
-    return book_report
 
-def main() :
-    book_report = generate_book_report("books/frankenstein.txt")
-    print(book_report)
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
-def sort_count(dictionary) :
-    return dictionary["count"]
 
-def filter_alphabet(dictionaries) :
-    alpha_dictionaries = []
-    for dictionary in dictionaries :
-        if dictionary["character"].isalpha() :
-            alpha_dictionaries.append(dictionary)
-    return alpha_dictionaries
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
+
 
 main()
